50aa05085709e16bc0efa82716f50c4985f664b6
joao <joao.bandeira@fattocs.com.br>
2024-08-21 18:19:54 -0300

    refactore: adicionando estilos e novo projeto

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Seleciona todas as seções
   const sections = document.querySelectorAll("section");
 
+  // Cria uma instância do IntersectionObserver
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
+          console.log(`Seção visível: ${entry.target.id}`);
+          // Se a seção está visível, adiciona a classe 'active'
           entry.target.classList.add("active");
         } else {
+          console.log(`Seção fora de vista: ${entry.target.id}`);
+          // Se a seção não está visível, remove a classe 'active'
           entry.target.classList.remove("active");
         }
       });
     },
-    { threshold: 0.5 }
+    { threshold: 0.5 } 
   );
 
+  // Observa cada seção
   sections.forEach((section) => {
     observer.observe(section);
   });
